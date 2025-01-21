f23b48f9b812681c2bc195ff26ef26f4d7f2a85c
Cale McNulty <cale.mcnulty@gauntletai.com>
2025-01-21 12:51:51 -0800

    fix imports

--- src/store/index.ts
+++ src/store/index.ts
@@ -12,3 +12,6 @@ export const store = configureStore({
     profiles: profilesReducer,
   },
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
