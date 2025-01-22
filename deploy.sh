963e5580fb4dbeeefcf02660a3dc6d11cec3db95
Cale McNulty <cale.mcnulty@gauntletai.com>
2025-01-21 20:02:40 -0800

    build

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@
 npm run build
 
 # Deploy to EC2 (replace with your values)
-scp -i /Users/calemcnulty/Workspace/perms/cale-ssh.pem -r dist/* ubuntu@44.218.104.132:/var/www/autocrm/
+scp -i /Users/calemcnulty/Workspace/perms/cale-ssh.pem -r dist/* ubuntu@44.218.104.132:~/autocrm/dist/
 
 # SSH into the server and set permissions
-ssh -i /Users/calemcnulty/Workspace/perms/cale-ssh.pem ubuntu@44.218.104.132 "sudo chown -R nginx:nginx /var/www/autocrm"
+ssh -i /Users/calemcnulty/Workspace/perms/cale-ssh.pem ubuntu@44.218.104.132 "sudo cp -r ~/autocrm/dist/* /var/www/autocrm/ && sudo chown -R www-data:www-data /var/www/autocrm"
 
