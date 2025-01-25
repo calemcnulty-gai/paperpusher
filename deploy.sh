1bfc88b31f94e58d97117d5ada24419e04c3c3d2
Cale McNulty <cale.mcnulty@gauntletai.com>
2025-01-24 19:59:06 -0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
 # Set environment variables for build
-export VITE_SUPABASE_URL="https://olfgwqwvvywhjmxhzmby.supabase.co"
-export VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sZmd3cXd2dnl3aGpteGh6bWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczOTc0NzQsImV4cCI6MjA1Mjk3MzQ3NH0.j_UD8dZUqSFgHDCus7AVkFhGDzIQw0DCKXJxNijy4mk"
+export VITE_SUPABASE_URL="https://gukvkyekmmdlmliomxtj.supabase.co"
+export VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1a3ZreWVrbW1kbG1saW9teHRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3NDg3MDYsImV4cCI6MjA1MzMyNDcwNn0.zlEKMxFEFlTBu4ePr_gSVes4s0ZMhTbQe-V9jxiQflg"
 
 # Build the project
 npm run build
 
 # Deploy to EC2 (replace with your values)
-scp -i /Users/calemcnulty/Workspace/perms/cale-ssh.pem -r dist/* ubuntu@44.218.104.132:~/autocrm/dist/
+scp -i /Users/calemcnulty/Workspace/perms/cale-ssh.pem -r dist/* ubuntu@44.218.104.132:~/paperpusher/dist/
 
 # SSH into the server and set permissions
-ssh -i /Users/calemcnulty/Workspace/perms/cale-ssh.pem ubuntu@44.218.104.132 "sudo cp -r ~/autocrm/dist/* /var/www/autocrm/ && sudo chown -R www-data:www-data /var/www/autocrm"
+ssh -i /Users/calemcnulty/Workspace/perms/cale-ssh.pem ubuntu@44.218.104.132 "sudo cp -r ~/paperpusher/dist/* /var/www/paperpusher/ && sudo chown -R www-data:www-data /var/www/paperpush"
