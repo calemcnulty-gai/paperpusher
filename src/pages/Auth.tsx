5516fc5910edf85bf8f04b5c943e886869226a8e
Cale McNulty <cale.mcnulty@gauntletai.com>
2025-01-31 20:21:14 -0800

    fix build

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -43,7 +43,6 @@ export default function AuthPage() {
           appearance={{ theme: ThemeSupa }}
           providers={["google"]}
           redirectTo={redirectTo}
-          onlyThirdPartyProviders
         />
       </div>
     </div>
